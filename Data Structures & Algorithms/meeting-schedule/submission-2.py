47649cf0db3ccb18d0de3c1531e03dea6b7fc32a
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-08-09 09:38:45 -0700

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        # Sort the intervals by the start time
+        intervals.sort(key=lambda x: x.start)
+
+        # Loop through intervals
+        for i in range(1, len(intervals)):
+            # if interval's start time is before the previous interval's end time: return False
+            if intervals[i].start < intervals[i-1].end:
+                return False
+        
+        return True
