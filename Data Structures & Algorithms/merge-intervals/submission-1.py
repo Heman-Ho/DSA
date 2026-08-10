10e79d18dbdd6ae88d06f682c468aebb35fa3c21
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-08-09 20:17:06 -0700

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        new_interval = intervals[0]
+        res = []
+
+        for i in range(1, len(intervals)):
+            # Case 1: the cur_interval doesn't overlap with the new_interval
+            if new_interval[1] < intervals[i][0]:
+                res.append(new_interval)
+                new_interval = intervals[i]
+            # Case 2: the interval overlaps with the new_interval
+            else:
+                new_interval[1] = max(new_interval[1], intervals[i][1])
+        res.append(new_interval)
+        return res
