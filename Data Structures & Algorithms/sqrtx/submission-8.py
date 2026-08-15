1d6a5228eae11533059d0b1484815d6680cdb757
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-08-15 09:27:33 -0700

    Add: sqrtx - submission-8

---
+++ Data Structures & Algorithms/sqrtx/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # The sqrt of any number x must be in the range [0, x]
+        # Use a binary search on that range to find the sqrt
+        l = 0
+        r = x
+
+        # if x = 10, res = 3
+
+        while l <= r:
+            m = (l + r) // 2
+            product = m * m
+            if product == x:
+                return m
+            elif product < x:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return r
