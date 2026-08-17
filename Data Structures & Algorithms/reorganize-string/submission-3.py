e571b9c58413bdb1aafcf147e781829c60cc7147
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-08-16 19:20:43 -0700

    Add: reorganize-string - submission-3

---
+++ Data Structures & Algorithms/reorganize-string/submission-3.py
@@ -0,0 +1,23 @@
+import heapq
+from collections import defaultdict, Counter
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        res = []
+        counts = Counter(s)
+
+        max_heap = [(-count, c) for c, count in counts.items()]
+        heapq.heapify(max_heap)
+
+        # create a priority queue for the next letter to add
+        # greedily choose the highest freq count letter 
+        buffer = (1, "#")
+
+        while max_heap:
+            count, char = heapq.heappop(max_heap)
+            res.append(char)
+        
+            if buffer[0] < 0:
+                heapq.heappush(max_heap, buffer)
+            buffer = (count+1, char)
+
+        return "".join(res) if len(res) == len(s) else ""
