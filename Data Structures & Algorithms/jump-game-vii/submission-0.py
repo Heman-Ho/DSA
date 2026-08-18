163c62919ea04316ce5a152dd880c5c76d77e4b4
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-08-17 21:57:39 -0700

    Add: jump-game-vii - submission-0

---
+++ Data Structures & Algorithms/jump-game-vii/submission-0.py
@@ -0,0 +1,25 @@
+from collections import deque
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        # We use a queue to hold the indices that we can jump to 
+        # Keep track of a max_reach variable 
+        max_reached = 0
+        q = deque([0])
+
+        while(q):
+            index = q.popleft()
+
+            # make the start index at least max_reached to avoid duplicate work
+            start = max(max_reached, index + minJump)
+            end = min(index + maxJump, len(s) - 1)
+            
+            # Add all the possible places we can jump to to the queue
+            for i in range(start, end + 1):
+                if s[i] == "0":
+                    if i == len(s) - 1:
+                        return True
+                    q.append(i)
+                    max_reached = i
+                
+        return False
+                    
