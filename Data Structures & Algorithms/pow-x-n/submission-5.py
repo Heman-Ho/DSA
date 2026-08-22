ec85c39d74fe64cb700047e75981e411fa84a0f7
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-08-22 16:14:25 -0700

    Add: pow-x-n - submission-5

---
+++ Data Structures & Algorithms/pow-x-n/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # x^n = x^(n//2) * x^((n + 1)//2)
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        if n == 1:
+            return x
+        if n < 0:
+            return 1/self.myPow(x, -n)
+        if n == 0:
+            return 1
+
+        half = self.myPow(x, n//2)
+        return half * half * (x if n % 2 == 1 else 1)
+
+        # what if x is negative? 
+        # -3^3 = -3^1 * -3^2 works
+        # -3^4 = -3^2 * -3^2 works
+
+        # what if n is negative
