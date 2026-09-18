f1b804ced752748e5de8c38a4b946b3dd3aa5ca5
Heman Ho <160170541+Heman-Ho@users.noreply.github.com>
2026-09-18 11:32:35 -0700

    Add: permutations-ii - submission-1

---
+++ Data Structures & Algorithms/permutations-ii/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        path = []
+        choices = Counter(nums)
+
+        def backtrack():
+            if len(path) == len(nums):
+                res.append(path.copy())
+                return
+            for choice, amount in choices.items():
+                if amount == 0:
+                    continue
+                path.append(choice)
+                choices[choice] -= 1
+                backtrack()
+                path.pop()
+                choices[choice] += 1
+
+        backtrack()
+        return res
+
+                
